d9097cf314a7a27635111f0b00f770dc5857d9bf
kajkitsu <norbert.waszkowiak@gmail.com>
2020-01-14 21:21:25 +0100

    troche refactoringu, wywalenie MV, osobna klasa na live data

--- app/src/main/java/pl/edu/wat/seswat/ui/teacher/selectAttendanceList/TSelectAttendanceListFragment.kt
+++ app/src/main/java/pl/edu/wat/seswat/ui/teacher/selectAttendanceList/TSelectAttendanceListFragment.kt
@@ -10,19 +10,57 @@ import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import pl.edu.wat.seswat.R
 import pl.edu.wat.seswat.database.FirestoreDataFunctions
+import pl.edu.wat.seswat.ui.teacher.TeacherData
+import pl.edu.wat.seswat.ui.teacher.TeacherMenuActivity
+
+
 
 class TSelectAttendanceListFragment : Fragment() {
 
-    private lateinit var tSelectAttendanceListViewModel: TSelectAttendanceListViewModel
+
     lateinit var recycler: RecyclerView
-    var adapterAttendance: RecyclerViewAdapterSelectAttendanceList = RecyclerViewAdapterSelectAttendanceList(MutableLiveData(),this.context)
+    lateinit var spinner: Spinner
+    lateinit var adapterAttendance: RecyclerViewAdapterSelectAttendanceList
+    lateinit var spinnerAdapter: ArrayAdapter<String>
+    lateinit var data: TeacherData
+    var subjectList: ArrayList<String> = ArrayList()
     val TAG = "SelectAttendanceListFr"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        data = (this.activity as TeacherMenuActivity).data
+
+        adapterAttendance = RecyclerViewAdapterSelectAttendanceList(data.allAttendanceLists,data.selectedAttendanceList,this.context)
+
+
+        subjectList=ArrayList()
+        if(data.allSubjects.value!=null)
+        for (subject in data.allSubjects.value!!){
+            subjectList.add(subject.name)
+        }
+        spinnerAdapter = ArrayAdapter(this.context!!, R.layout.support_simple_spinner_dropdown_item, subjectList)
+        Log.d("DUPA1",data.allSubjects.value.toString())
+        Log.d("DUPA1",subjectList.toString())
+
+
+
+
+        data.allAttendanceLists.observe(this, Observer {
+            adapterAttendance.setList(it)
+        })
+
+
+
+
+    }
+
 
 
     override fun onCreateView(
@@ -30,31 +68,42 @@ class TSelectAttendanceListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        tSelectAttendanceListViewModel =
-            ViewModelProviders.of(this).get(TSelectAttendanceListViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_t_select_attendance_list, container, false)
 
-        val spinnerSubject: Spinner = root.findViewById(R.id.spinner_lecture)
-        recycler = root.findViewById(R.id.recycler_view_select_list)
+        spinner = root.findViewById(R.id.spinner_lecture)
 
 
+//        if(data.allSubjects.value!=null){
+//            for (subject in data.allSubjects.value!!){
+//                subjectList.add(subject.name.toString())
+//                Log.d("DUPA",subject.name.toString())
+//            }
+//        }
 
-        FirestoreDataFunctions().getAllSubjectList().observe(this, Observer {
-            if(it!=null) {
-                Log.d(TAG, it.toString())
 
-                val listArray: ArrayList<String> = ArrayList()
-                for (subject in it){
-                    listArray.add(subject.name.toString())
-                }
+        Log.d("DUPA2",data.allSubjects.value.toString())
+        Log.d("DUPA2",subjectList.toString())
 
-                val spinnerAdapter: ArrayAdapter<String> =
-                    ArrayAdapter(this.context!!, R.layout.support_simple_spinner_dropdown_item, listArray)
-                spinnerSubject.adapter = spinnerAdapter
+        data.allSubjects.observe(this, Observer {
+            Log.d("DUPA3", it.toString())
+            subjectList=ArrayList()
+            for (subject in it){
+                subjectList.add(subject.name)
             }
+            spinnerAdapter = ArrayAdapter(this.context!!, R.layout.support_simple_spinner_dropdown_item, subjectList)
+            spinner.adapter=spinnerAdapter
         })
 
-        initRecyclerView()
+        spinner.adapter = ArrayAdapter(this.context!!, R.layout.support_simple_spinner_dropdown_item, subjectList)
+
+        recycler = root.findViewById(R.id.recycler_view_select_list)
+        recycler.setAdapter(adapterAttendance)
+        recycler.setLayoutManager(LinearLayoutManager(this.context))
+
+
+//        initRecyclerView()
+//        updateRecyclerView()
+
 
 
 
@@ -65,28 +114,28 @@ class TSelectAttendanceListFragment : Fragment() {
         return root
     }
 
-    fun initRecyclerView(){
-        var mAuth = FirebaseAuth.getInstance()
-        recycler.setAdapter(adapterAttendance)
-        recycler.setLayoutManager(LinearLayoutManager(this.context))
-
-        mAuth.currentUser?.uid?.let { FirestoreDataFunctions().getTeacherAttendanceList(it).observe(this, Observer {
-            Log.d(TAG," FirestoreDataFunctions().getTeacherAttendanceList(it).observe")
-            adapterAttendance.setList(it)
-        })
-
-        }
-
-
-
-    }
-
-
-    fun updateRecyclerView(){
-        adapterAttendance =
-            RecyclerViewAdapterSelectAttendanceList(MutableLiveData(),this.context)
-        recycler.setAdapter(adapterAttendance)
-        recycler.setLayoutManager(LinearLayoutManager(this.context))
-
-    }
+//    fun initRecyclerView(){
+//        var mAuth = FirebaseAuth.getInstance()
+//        recycler.setAdapter(adapterAttendance)
+//        recycler.setLayoutManager(LinearLayoutManager(this.context))
+//
+//        mAuth.currentUser?.uid?.let { FirestoreDataFunctions().getTeacherAttendanceList(it).observe(this, Observer {
+//            Log.d(TAG," FirestoreDataFunctions().getTeacherAttendanceList(it).observe")
+//            adapterAttendance?.setList(it)
+//        })
+//
+//        }
+//
+//
+//
+//    }
+//
+//
+//    fun updateRecyclerView(){
+//        adapterAttendance =
+//            RecyclerViewAdapterSelectAttendanceList(MutableLiveData(),(this.activity as TeacherMenuActivity).selectedAttendanceList,this.context)
+//        recycler.setAdapter(adapterAttendance)
+//        recycler.setLayoutManager(LinearLayoutManager(this.context))
+//
+//    }
 }
